e51d58646c9ca8b2b19c7fe675b350a4bf4fccbe
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2015-09-05 22:11:00 -0400

    Spinner from spinjs + Css fixes

--- extension/postAndConvert.js
+++ extension/postAndConvert.js
@@ -1,35 +1,55 @@
 /**
  * Created by yusaira-khan on 13/08/15.
  */
-
 var url = "http://localhost:8080/api/";
+function handleError() {
+  console.error('An error occured');
+  spinner.stop();
+  target.style.display='none';
+  document.getElementById('error').style.display = 'block';
+  document.getElementById('original-text-button').style.display='none';
+}
 
 var x = new XMLHttpRequest();
 x.open('POST', url);
 x.onload = function () {
   var response = x.response;
   if (!response) {
-    console.error('error');
+    handleError();
     return;
   }
+  target.style.display='none';
+  spinner.stop();
   console.log(response);
-  document.getElementById('spinner').style.display='none';
-  document.getElementById('text').innerHTML += "<br/><div style='color:red;'>" + response + '<div>';
+  document.getElementById('result').innerHTML += "<br/><div style='color:red;'>" + response + '<div>';
+
 };
-x.onerror = function () {
-  console.error('Network error.');
-};
+x.onerror = handleError;
 //TODO:fix css
 //TODO:get an icon with bangla
 //TODO:organise the extension better
 //TODO: standalone chrome app without server with selectable pronunciation?
-chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-  chrome.tabs.sendMessage(tabs[0].id,
-    {"message": "selected_text"}, function (selectedText) {
-      console.log(selectedText);
-      document.getElementById('text').innerHTML = "sending \"" + selectedText + "\"\n";
-      x.send(selectedText);
 
-    });
+function getSelectedText(callback) {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {"message": "selected_text"}, function (selectedText) {
+        console.log(selectedText);
+        callback(selectedText);
+      });
+  });
+}
+
+function displayOriginalText() {
+  getSelectedText(function(selectedText){
+    document.getElementById('original').innerHTML = "Converted:<br/> &quot;" + selectedText + "&quot;";
+  });
+}
+
+getSelectedText(function(selectedText){
+      x.send(selectedText);
 });
 
+window.onload=function() {
+  var original = document.getElementById('original-text-button');
+  original.addEventListener("click", displayOriginalText);
+};
